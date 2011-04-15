fdd365aa83c1a2763290b1e5bddb0adb82a95dca
ppavlidis <paul@chibi.ubc.ca>
2011-04-15 20:37:07 +0000

    - Model change: add column in QuantitationType to indicate whether it is batch corrected - Improvements to output and options for outlier CLI.

--- src/main/java/chibi/gemmaanalysis/OutlierDetectionCli.java
+++ src/main/java/chibi/gemmaanalysis/OutlierDetectionCli.java
@@ -24,13 +24,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Collection;
+import java.util.Date;
 
+import ubic.gemma.analysis.preprocess.OutlierDetails;
 import ubic.gemma.analysis.preprocess.OutlierDetectionService;
-import ubic.gemma.apps.DifferentialExpressionAnalysisCli;
 import ubic.gemma.apps.ExpressionExperimentManipulatingCLI;
 import ubic.gemma.model.expression.bioAssay.BioAssay;
 import ubic.gemma.model.expression.experiment.BioAssaySet;
 import ubic.gemma.model.expression.experiment.ExpressionExperiment;
+import ubic.gemma.util.DateUtil;
 
 /**
  * Checks experiments for outliers
@@ -57,6 +59,10 @@ public class OutlierDetectionCli extends ExpressionExperimentManipulatingCLI {
 
     boolean useRegression = false;
 
+    double thresholdFraction = 0.9;
+
+    int quantile = 15;
+
     /**
      * @param fileName
      * @return
@@ -78,6 +84,12 @@ public class OutlierDetectionCli extends ExpressionExperimentManipulatingCLI {
 
         this.addOption( REGRESSION_OPT, false, "Set to use regression to attempt to "
                 + "subtract the effects of experimental factors before looking for outliers." );
+
+        this.addOption( "quantile", true,
+                "Set the quantile of correlations that would be considered 'suspiciously low'" );
+
+        this.addOption( "thresholdFraction", true, "Set the fraction of samples which must have "
+                + "correlations < quantile for a sample to be considered an outlier." );
     }
 
     /*
@@ -94,7 +106,11 @@ public class OutlierDetectionCli extends ExpressionExperimentManipulatingCLI {
 
         try {
             summaryFile = initOutputFile( "outlier.summary.txt" );
-            summaryFile.write( "EEID\tEENAME\tBAID\tBANAME\n" );
+            summaryFile.write( "# Created by Gemma OutlierDetectionCli\n" );
+            summaryFile.write( "# " + DateUtil.convertDateToString( new Date() ) + "\n" );
+            summaryFile.write( "# Quantile = " + quantile + "\n" );
+            summaryFile.write( "# Threshold fraction = " + String.format( "%.2f", this.thresholdFraction ) + "\n" );
+            summaryFile.write( "EEID\tEENAME\tBAID\tBANAME\tSCORE\tTHRESHOLDCORR\n" );
 
             for ( BioAssaySet bas : this.expressionExperiments ) {
                 if ( !( bas instanceof ExpressionExperiment ) ) {
@@ -115,17 +131,24 @@ public class OutlierDetectionCli extends ExpressionExperimentManipulatingCLI {
         return null;
     }
 
+    /**
+     * @param ee
+     */
     protected void processExperiment( ExpressionExperiment ee ) {
         try {
             StringBuilder sb = new StringBuilder();
 
-            Collection<BioAssay> outliers = this.outlierDetectionService.identifyOutliers( ee, this.useRegression );
+            Collection<OutlierDetails> outliers = this.outlierDetectionService.identifyOutliers( ee,
+                    this.useRegression, this.quantile, this.thresholdFraction );
 
             String baseS = ee.getId() + "\t" + ee.getShortName();
 
             // TODO we might get more details here (like the correlation)
-            for ( BioAssay bioAssay : outliers ) {
-                String line = baseS + "\t" + bioAssay.getId() + "\t" + bioAssay.getName();
+            for ( OutlierDetails outlier : outliers ) {
+                BioAssay bioAssay = outlier.getBioAssay();
+                double score = outlier.getScore();
+                String line = baseS + "\t" + bioAssay.getId() + "\t" + bioAssay.getName() + "\t"
+                        + String.format( "%.2f\t%.2f", score, outlier.getThresholdCorrelation() );
                 sb.append( line + "\n" );
                 log.info( "Outlier: " + line );
             }
